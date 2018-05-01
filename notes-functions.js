eb536b1d63895e7a7185cb888c6d13ef0a6bd2b1
Oliver De La Via <odelavia@gmail.com>
2018-05-01 17:37:24 -0400

    added and setup removeNote button

--- notes-functions.js
+++ notes-functions.js
@@ -9,21 +9,43 @@ const getSavedNotes = function () {
     }
 }
 
-// save notes to application
+// save notes to localStorage
 const saveNotes = function(notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+//remove a note from the list
+const removeNote = function(id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
 // generate the dom structure for note
 const generateNoteDOM = function (note) {
-    const noteEl = document.createElement('p')
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('span')
+    const button = document.createElement('button')
 
-    if(note.title.length > 0) {
-        noteEl.textContent = note.title
+    button.textContent = 'x'
+    noteEl.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
+
+    if (note.title.length > 0) {
+        textEl.textContent = note.title
     } else {
-        noteEl.textContent = 'Untitled'
+        textEl.textContent = 'Untitled'
     }
-
+    noteEl.appendChild(textEl)
+    
     return noteEl
 }
 
